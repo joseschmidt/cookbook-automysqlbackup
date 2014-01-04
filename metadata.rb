1a3a743b418d4d7ab5faee3bcd93dcd632e6f134
Doc Walker <doc.walker@jameshardie.com>
2014-01-03 18:04:57 -0600

    Add metadata dependency for build-essential

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,7 @@
 name              'automysqlbackup'
 version           '0.1.0'
 
+depends           'build-essential'
 depends           'database'
 depends           'helpers'
 depends           'mysql'
