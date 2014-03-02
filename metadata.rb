640cd5ffbc2b1f47fc3bd8129a26d4a7093a0c86
Doc Walker <doc.walker@jameshardie.com>
2014-03-02 07:20:27 -0600

    Update metadata.rb and standardize layout

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,32 @@
 # encoding: utf-8
 name              'automysqlbackup'
+maintainer        'James Hardie Building Products, Inc.'
+maintainer_email  'doc.walker@jameshardie.com'
+description       'Installs and configures automysqlbackup ' \
+                  '(MySQL backup tool).'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+license           'Apache 2.0'
 version           '0.2.0'
 
+#--------------------------------------------------------------------- recipes
+recipe            'automysqlbackup',
+                  'Installs and configures automysqlbackup'
+
+#------------------------------------------------------- cookbook dependencies
 depends           'chef-sugar'
 depends           'database'
 depends           'mysql'
+
+#--------------------------------------------------------- supported platforms
+# tested
+supports          'centos'
+
+# platform_family?('rhel'): not tested, but should work
+supports          'amazon'
+supports          'oracle'
+supports          'redhat'
+supports          'scientific'
+
+# platform_family?('debian'): not tested, but may work
+supports          'debian'
+supports          'ubuntu'
